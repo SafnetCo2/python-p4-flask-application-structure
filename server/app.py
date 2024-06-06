4904fc5b001703d3e1624dd303807fd9b0186b86
Jospine <nzi19001@byui.edu>
2024-06-06 11:30:52 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,28 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>These profile is for {username}</h1>'
+
+    
+if __name__=='__main__':
+    app.run()
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to the homepage!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
+
+>>>>>>> fee8a5bb73ade7588138e1f2fed1f387fca5c449
